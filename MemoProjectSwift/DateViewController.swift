f5598e653bcb3665379a785c898f31bae11ba1b4
atlaf04 <143972478+atlaf04@users.noreply.github.com>
2024-04-21 23:20:43 -0400

    added methods  to memo view controller

--- MemoProjectSwift/DateViewController.swift
+++ MemoProjectSwift/DateViewController.swift
@@ -7,15 +7,31 @@
 
 import UIKit
 
-class DateViewController: UIViewController {
+protocol DateControllerDelegate: AnyObject {
+    func dateChanged(date: Date)
+}
 
+class DateViewController: UIViewController {
+    
+    weak var delegate: DateControllerDelegate?
+    
+    @IBOutlet weak var dtpDate: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let saveButton: UIBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.save,
+                            target: self,
+                            action: #selector(saveDate))
+        self.navigationItem.rightBarButtonItem = saveButton
+        self.title = "Pick Birthdate"
     }
     
-
+    @objc func saveDate(){
+        self.delegate?.dateChanged(date: dtpDate.date)
+        self.navigationController?.popViewController(animated: true)
+    }
     /*
     // MARK: - Navigation
 
@@ -27,3 +43,17 @@ class DateViewController: UIViewController {
     */
 
 }
+// You now have a new file in your project DateViewController, which will be used to manage any actions on the Birthdate scene. This file will contain the DateControllerDelegate interface, as well as a reference to a delegate that it can use to specify the date that was picked.
+// Switch to DateViewController.swift and add lines 3–5 in Listing 11.13.
+          //This sets up the delegate protocol and specifies that any class adopting this protocol must also implement the dateChanged method. What the class chooses to have the dateChanged method do is up to the developer. In this case, the Date Controller calls this method on its delegate anytime the date changes. Note that the protocol uses the class keyword to specify that it only is available to be applied to classes—not to structs and enumerations.
+    // For the subview to call back to the main view, it needs a reference to the main view. You do this by setting up a property that will have a reference to the main view. Add line 10 in Listing 11.13. The delegate property is of the same type as the protocol you just created. This will allow any kind of controller (or other object) to take advantage of the Date Controller to set a date. In some instances, a main controller may not set itself as a delegate of the Date Controller, so the delegate variable is set to weak and the type is optional, as specified by the question mark. See Appendix B for more on how to declare variables in Swift.
+
+
+
+
+
+// Step 3: Specify That Main View Will Implement Delegate Protocol Switch to ContactsViewController.swift and specify that the class will conform to DateControllerDelegate by adding it to the list of interfaces in the class declaration:
+
+// class ContactsViewController: UIViewController, UITextFieldDelegate, DateControllerDelegate {
+
+
--- MemoProjectSwift/MemoViewController.swift
+++ MemoProjectSwift/MemoViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MemoViewController: UITableViewController {
+class MemoViewController: UIViewController {
 
     @IBOutlet weak var sgmtEdit: UISegmentedControl!
     
@@ -22,80 +22,58 @@ class MemoViewController: UITableViewController {
     @IBOutlet weak var highButton: UIButton!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+            super.viewDidLoad()
+            
+            // Setup initial UI state
+            initUI()
+        }
+        
+        // MARK: - UI Configuration
+        
+        func initUI() {
+            // Configure initial state of UI elements
+            
+            // Set up segmented control action
+            sgmtEdit.addTarget(self, action: #selector(editModeChanged(_:)), for: .valueChanged)
+            
+            // Set up date button action
+            dateButton.addTarget(self, action: #selector(changeDate(_:)), for: .touchUpInside)
+        }
+        
+        // MARK: - Actions
+        
+        @objc func editModeChanged(_ sender: UISegmentedControl) {
+            // Handle segmented control value changed event
+            
+            switch sender.selectedSegmentIndex {
+            case 0:
+                // Editing mode off
+                enableEditing(false)
+            case 1:
+                // Editing mode on
+                enableEditing(true)
+            default:
+                break
+            }
+        }
+        
+        @objc func changeDate(_ sender: UIButton) {
+            // Handle date button tapped event
+            // Implement your date change logic here
+        }
+        
+        // MARK: - Helper Methods
+        
+        func enableEditing(_ enabled: Bool) {
+            // Enable or disable editing for text fields and buttons
+            
+            txtSubject.isEnabled = enabled
+            txtMemo.isEnabled = enabled
+            dateButton.isEnabled = enabled
+            mediumButton.isEnabled = enabled
+            lowButton.isEnabled = enabled
+            highButton.isEnabled = enabled
+            
+            // Additional UI updates as needed
+        }
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
